ec6740444cb0a3928d702be6533139a7d8268a4e
VoxFrameworks <vdmofficially@outlook.com>
2026-02-13 21:12:03 -0500

    Increase player vertical motion on key press

--- jetpack.js
+++ jetpack.js
@@ -2,7 +2,7 @@ ModAPI.require("player");
 
 ModAPI.addEventListener("key", (event) => {
   if (event.key === 35) { // Key code for 'h'
-ModAPI.player.motionY += 0.2;
+ModAPI.player.motionY += 0.5;
 ModAPI.updateComponent("player");
   }
 });
